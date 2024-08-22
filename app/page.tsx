7d8aa76744bba4ca959fef56859eba847af656f9
yijie37 <yijiehere@163.com>
2024-08-22 13:03:12 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -39,8 +39,8 @@ export default function Home() {
         userData ?
           (
             <>
-              <h1 className="text-2xl font-bold mb-4">User Data</h1>
-              <ul>
+              <h1 className="text-2xl font-bold mb-4 items-center">User Data</h1>
+              <ul className="mb-4 text-center">
                 <li>ID: {userData.id}</li>
                 <li>User Name: {userData.first_name}</li>
                 <li>50000 $PPPPP</li>
@@ -50,14 +50,14 @@ export default function Home() {
                 <li>Language Code: {userData.language_code}</li>
                 <li>Is Premium: {userData.is_premium}</li> */}
               </ul>
-              <>
+              <div className="flex flex-col items-center space-y-4 mt-4">
                 <button onClick={handleButton1Click} className="px-4 py-2 bg-blue-500 text-white rounded">
                   Invite Friends
                 </button>
                 <button onClick={handleButton2Click} className="px-4 py-2 bg-blue-500 text-white rounded">
                   Claim   
                 </button>
-              </>
+              </div>
             </>
           ) :
           (
